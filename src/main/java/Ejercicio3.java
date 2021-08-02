4c6ee7e91c7100d46bd1ad7d1abb0fa63672345d
miluAstete <miluskaevy272@gmail.com>
2021-08-02 13:11:57 -0500

    Modificado

--- src/main/java/Ejercicio3.java
+++ src/main/java/Ejercicio3.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Ejercicio3 {
 
-    public static void main(){
+    public static void main(String args []){
         Scanner input = new Scanner(System.in);
         System.out.println("Ingrese un mensaje");
         String mensaje = input.next();
-        System.out.println(mensaje);
+        System.out.println("Bienvenido el mensaje es: "mensaje);
     }
 
 }
