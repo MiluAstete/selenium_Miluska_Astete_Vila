9d6ab71f8601f530f0119d6701d67f78f5c86c31
miluAstete <miluskaevy272@gmail.com>
2021-08-02 12:58:31 -0500

    Primer ejercicio intellij

--- src/main/java/Ejercicio3.java
+++ src/main/java/Ejercicio3.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+
 public class Ejercicio3 {
-    
+
+    public static void main(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese un mensaje");
+        String mensaje = input.next();
+        System.out.println(mensaje);
+    }
 
 }
